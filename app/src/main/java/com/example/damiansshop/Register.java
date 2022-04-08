b42bf745940269e2d45e470adce9c2c1e05bb75a
Damianglu <damian.gluszkiewicz123@gmail.com>
2022-04-08 12:45:35 +0100

    Added in Login class and initialized all fields from Register

--- app/src/main/java/com/example/damiansshop/Register.java
+++ app/src/main/java/com/example/damiansshop/Register.java
@@ -3,6 +3,7 @@ package com.example.damiansshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,5 +21,25 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Initializing TextViews
+        mRegisterTextView = findViewById(R.id.RegisterTextView);
+        mLoginTextView = findViewById(R.id.LoginTextView);
+        //Initializing EditTexts
+        mRegisterName = findViewById(R.id.RegisterName);
+        mRegisterEmail = findViewById(R.id.RegisterEmail);
+        mRegisterAddress = findViewById(R.id.RegisterAddress);
+        mRegisterCard = findViewById(R.id.RegisterCard);
+        mRegisterPassword = findViewById(R.id.RegisterPassword);
+        //Initializing Buttons
+        mRegisterButton = findViewById(R.id.RegisterButton);
+
+
+        mLoginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_login);
+            }
+        });
     }
 }
